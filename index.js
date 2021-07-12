6ec7f4669699f1d03fefb2dcbe357d07c976512a
Gabriel de Oliveira Rigo <gabrieloliveirarigo13@gmail.com>
2021-07-12 00:26:46 -0300

    feature: add missing index.js file

---
+++ index.js
@@ -0,0 +1,17 @@
+const customExpress = require("./config/customExpress");
+const connection = require('./infraestrutura/connection');
+const Tabelas = require('./infraestrutura/tabelas');
+const port = 3000;
+const app = customExpress();
+
+connection.connect((error) => {
+    if(error) {
+        console.log("erro: ", error)
+    } else {
+        Tabelas.init(connection);
+        app.listen(port, () => {
+            console.log("Servidor rodando com sucesso")
+        });
+        
+    }
+})
